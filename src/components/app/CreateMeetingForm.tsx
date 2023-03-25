33636561dcff2c986c5e9d402f97feb6c8ca4a52
Edward Nguyen <monokaijs@gmail.com>
2023-03-25 15:21:20 +0700

    🎨 fix bottom padding for meeting creation sheet

--- src/components/app/CreateMeetingForm.tsx
+++ src/components/app/CreateMeetingForm.tsx
@@ -3,10 +3,12 @@ import { StyleSheet, View } from "react-native";
 import { Text } from "react-native-paper";
 import { useAppTheme } from "@configs/theme/theme.config";
 import { getSize } from "@utils/reponsive.utils";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
 export const CreateMeetingForm = () => {
   const {colors} = useAppTheme();
-  const styles = useStyles(colors);
+  const insets = useSafeAreaInsets();
+  const styles = useStyles(colors, insets);
 
   return (
     <View style={styles.outer}>
@@ -19,10 +21,11 @@ export const CreateMeetingForm = () => {
   )
 };
 
-const useStyles = (colors) => {
+const useStyles = (colors, insets) => {
   return StyleSheet.create({
     outer: {
-      padding: getSize.m(16)
+      padding: getSize.m(16),
+      paddingBottom: getSize.m(16) + insets.bottom,
     },
     title: {
       fontSize: getSize.m(32),
